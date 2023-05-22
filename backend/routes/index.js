3b5de67f099f6908bf98da2c55e64eee13fddb91
Krzysztof Bański <krzys0100@wp.pl>
2023-05-22 20:36:21 +0200

    cleanup

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,16 +1,11 @@
-const status = require('../src/health/routes');
 const users = require('../src/users/routes');
 const books = require('../src/books/routes');
-// const validateAuth = require('../middlewares/validateAuth');
-// const getData = require('../middlewares/getData');
 
 require('../src/users_books/models/index').init();
 
 module.exports = (app) => {
-  app.use('/status', status);
   app.use('/users', users);
   app.use('/books', books);
-  // app.use('/users', validateAuth.checkIfAuthenticated, getData.getGeoip, users);
   app.use
   app.use('*', (req, res) => {
     res.send('Not found!!!');
